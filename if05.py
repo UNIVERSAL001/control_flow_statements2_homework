f7e16bfd2de32c4726aedb69e84a21b8f9805cff
Murodjon <developer1982002@gmai.com>
2022-10-26 19:29:20 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,24 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n%10
+    n//=10
+    x2=n%10
+    n//=10
+    x3=n%10
+    n//=10
+    x4=n%10
+    n//=10
+    x5=n%10
+    m = x1
+    if m <x1:
+        m=x1
+    if m <x2:
+        m=x2
+    if m <x3:
+        m=x3
+    if m <x4:
+        m=x4
+    if m <x5:
+        m=x5
+    return m
