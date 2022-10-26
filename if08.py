9c63d67a515d4f077f2cc5b14f689ff495bdf226
Murodjon <developer1982002@gmai.com>
2022-10-26 19:37:45 +0500

    08

--- if08.py
+++ if08.py
@@ -14,4 +14,18 @@ def main(number):
     Returns:
         str: return answer.
     """
-    return
+    x = number%7
+    if x==0:
+        return "Sunday"
+    elif x==1:
+        return "Monday"
+    elif x==2:
+        return "Tuesday"
+    elif x==3:
+        return "Wednesday"
+    elif x==4:
+        return "Thursday"
+    elif x==5:
+        return "Friday"
+    elif x==6:
+        return "Saturday"
